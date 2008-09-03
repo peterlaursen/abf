32b49e4fcb6c7fbb46fcb0c1feefe88d4df04949
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2008-09-03 15:17:39 +0000

    Updated DaisyInfo to follow the standard.
    git-svn-id: svn://mosedal.net/abf/code/trunk@45 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- daisyinfo/daisyinfo.cpp
+++ daisyinfo/daisyinfo.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "daisyinfo.h"
 using namespace std;
+void Show(ifstream& fin, const char* Text, const char* Name) {
+string Value = ExtractInfo(fin, Name);
+if (Value == "notfound") return;
+cout << Text << Value << endl;
+}
 string ExtractInfo(ifstream& fin, string& Name) {
 string Line;
 int Position = 0;
@@ -9,9 +15,8 @@ string MetaString("meta");
 while (1) {
 getline(fin, Line);
 if (Line.find("</head>") != string::npos) {
-cout << "Data element not found." << Name;
 fin.seekg(0, ios::beg);
-return Name;
+return string("notfound");
 }
 int Position = Line.find("meta");
 if (Position == string::npos) continue;
@@ -37,7 +42,7 @@ break;
 string ExtractInfo(ifstream& fin, const char* Name) { return ExtractInfo(fin, string(Name)); }
 int main(int argc, char* argv[]) {
 if (argc != 2) {
-cout << "Usage: " << argv[0] << " <path>" << endl;
+cout << "Usage: " << argv[0] << " <path>" << endl << "where <path> is the directory containing your Daisy book." << endl;
 return 1;
 }
 ifstream fin;
@@ -49,13 +54,36 @@ cout << "File not found." << endl;
 return 1;
 }
 // Here is some info from the daisy book.
-cout << "Author: " << ExtractInfo(fin, string("dc:creator")) << endl;
-cout << "Title: " << ExtractInfo(fin, string("dc:title")) << endl;
+Show(fin, "Contributors to this book: ", "dc:contributor");
+Show(fin, "Author: ", "dc:creator");Show(fin, "Subject of Book: ", "dc:coverage");
+Show(fin, "Date Published: ", "dc:date");
+Show(fin, "Description: ", "dc:description");
+Show(fin, "Format of Daisy Book: ", "dc:format");
+Show(fin, "Format of Daisy Book: ", "ncc:format");
+Show(fin, "Unique Identifier: ", "dc:identifier");
+Show(fin, "Unique Identifier: ", "ncc:identifier");
+Show(fin, "Language: ", "dc:language");
+Show(fin, "Publisher: ", "dc:publisher");
+Show(fin, "Related Resource: ", "dc:relation");
+Show(fin, "Special Legal Issues: ", "dc:rights");
+Show(fin, "Print Source: ", "dc:source");
+Show(fin, "Subject: ", "dc:subject");
+Show(fin, "Title: ", "dc:title");
+Show(fin, "Daisy Book Type: ", "dc:type");
+Show(fin, "XML file encoding: ", "ncc:charset");
+Show(fin, "Number of Levels: ", "ncc:depth");
+Show(fin, "Number of Files in this book: ", "ncc:files");
+Show(fin, "Footnotes: ", "ncc:footnotes");
+Show(fin, "Software that generated this daisy book: ", "ncc:generator");
+Show(fin, "Size of Book (in kb): ", "ncc:kByteSize");
+Show(fin, "Page Count: ", "ncc:maxPageNormal");
+Show(fin, "Type of Daisy book: ", "ncc:multimediaType");
+Show(fin, "Narrator: ", "ncc:narrator");
+Show(fin, "Front Page(s): ", "ncc:pageFront");
+Show(fin, "Front Page(s): ", "ncc:page-front");
+Show(fin, "Normal pages occuring: ", "ncc:pageNormal");
+Show(fin, "Normal pages occuring: ", "ncc:page-normal");
+Show(fin, "Special Pages: ", "ncc:pageSpecial");
+Show(fin, "Special Pages: ", "ncc:page-special");
 
-cout << ExtractInfo(fin, string("dc:title")) << endl;
-cout << "Publishing Organisation: " << ExtractInfo(fin, string("dc:publisher")) << endl;
-cout << "Size of book in kilobytes: " << ExtractInfo(fin, "ncc:kByteSize") << endl;
-cout << "Narrator: " << ExtractInfo(fin, "ncc:narrator") << endl;
-cout << "Publishing Date: " << ExtractInfo(fin, "dc:date") << endl;
-cout << "Language: " << ExtractInfo(fin, "dc:language");
 }
