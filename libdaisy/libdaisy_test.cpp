ef3ecda59c2e10cc9c3324fa65bcd5d0be01d349
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2008-09-24 07:18:24 +0000

    Added my small test program for libdaisy.
    git-svn-id: svn://mosedal.net/abf/code/trunk@83 3c19b6e9-aa5f-dd11-88a4-001302d38fef

---
+++ libdaisy/libdaisy_test.cpp
@@ -0,0 +1,18 @@
+#include "libdaisy.h"
+#include <iostream>
+using namespace std;
+using namespace ABF;
+int main(int argc, char* argv[]) {
+if (argc != 2) {
+cerr << "Error, only one argument must be passed to this program." << endl;
+return 1;
+}
+Daisy D(argv[1], false);
+if (!D.Open()) {
+cout << "Something went wrong during opening." << endl;
+return 1;
+}
+cout << D.GetMetaInfo() << endl;
+string Meta("dc:date");
+cout << D.ExtractMetaInfo(Meta) << endl;
+}
