cebb36275afd4f56883b91a1a50339281fce931d
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2008-09-27 13:20:27 +0000

    Hopefully fixed the problem on GCC. Changed libdaisy_test.cpp.
    git-svn-id: svn://mosedal.net/abf/code/trunk/libdaisy@89 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- libdaisy_test.cpp
+++ libdaisy_test.cpp
@@ -1,29 +1,23 @@
 #include "libdaisy.h"
 #include <iostream>
-using namespace std;
-using namespace ABF;
+#include <cstdio>
+using namespace ABF;using namespace std;
+
 int main(int argc, char* argv[]) {
-if (argc != 2) {
-cerr << "Error, only one argument must be passed to this program." << endl;
-return 1;
-}
 Daisy D(argv[1], false);
-if (!D.Open()) {
-cout << "Something went wrong during opening." << endl;
-return 1;
-}
-string Meta("dc:date");
-cout << D.ExtractMetaInfo(Meta) << endl;
+D.Open();
 cout << D.GetMP3FileName() << endl;
-while (1) {
-const char* Value = GetNextAudioFile(D);
-if (!Value) break;
+while (D.OpenSmil()) {// Should open the next
+const char* Value = D.GetMP3FileName();
+if (Value == "notfound") break;
 cout << Value << endl;
 FILE* File = fopen(Value, "rb");
-if (!File) cout << "Cannot open file." << endl;
+if (!File) cout << "File not found.";
 else {
 fclose(File);
-cout << "File was opened successfully." << endl;
+cout << Value << " opened successfully." << endl;
 }
 }
 }
+
+
