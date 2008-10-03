9bc179fbfe599658656a0425d4510a7947ebc6e6
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2008-10-03 12:07:51 +0000

    Added the rest of the decoder, it seems to work now.;
    git-svn-id: svn://mosedal.net/abf/code/trunk/decoder@103 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- libdecoder.cpp
+++ libdecoder.cpp
@@ -1,7 +1,9 @@
 #include <libdaisy.h>
 #include <cstdio>
 #include "libdecoder.h"
+#include <audiere.h>
 namespace ABF {
+using namespace audiere;
 Decoder::Decoder(Daisy& _Book, const char* Filename): _Daisy(_Book) {
 _Output = fopen(Filename, "wb");
 if (!_Output) _Open = false;
@@ -15,8 +17,22 @@ const char* Filename = _Daisy.GetMP3FileName();
 // Open the audio file we have just been given.
 SampleSource* Source = OpenSampleSource(Filename);
 if (!Source) return false;
+const int ReadSize = 4096;
+int Channels, SampleRate;
+SampleFormat SF;
+Source->getFormat(Channels, SampleRate, SF);
+int FrameSize = GetSampleSize(SF) * Channels;
+printf("Frame size: %d\r\n", FrameSize);
+int FramesRead = 0;
+short Buffer[ReadSize];
 while (1) {
-// To be continued from this point on.
+int FramesRead = Source->read(sizeof(Buffer) / 2, Buffer);
+if (FramesRead <= 0) {
+printf("Finished decoding %s\r\n", Filename);
+break;
+}
+fwrite(Buffer, sizeof(short), ReadSize, _Output);
+}
 }
 return true;
 }
