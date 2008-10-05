ca9056bac0480e5f6559b921f1a55240c4405782
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2008-10-05 16:20:47 +0000

    Added threading support to DaisyPlayer.
    git-svn-id: svn://mosedal.net/abf/code/trunk@116 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- daisyplayer/daisyplayer.cpp
+++ daisyplayer/daisyplayer.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
+#include <windows.h>
+#include <process.h>
 #include <libdaisy.h>
 #include <audiere.h>
+#include <conio.h>
 using namespace std;
 using namespace audiere;
 using namespace ABF;
-int main(int argc, char* argv[]) {
-Daisy D(argv[1], false);
+bool Quit = false;
+char* CommandString;
+void Input();
+void Thread(void*) {
+Daisy D(CommandString, false);
 D.Validate();
 if (!D.IsValid()) {
 cout << "Error, this does not appear to be a Daisy book." << endl;
-return 1;
+Quit = true;
+return;
 }
 D.Open();
 D.OpenSmil();
 AudioDevicePtr Device = OpenDevice();
 OutputStreamPtr Stream = OpenSound(Device, D.GetMP3FileName(), true);
 Stream->play();
-while (1) {
+while (!Quit) {
 if (Stream->isPlaying()) continue;
-D.OpenSmil();
+if (!D.OpenSmil()) {
+Quit = true;
+break;
+}
 Stream = OpenSound(Device, D.GetMP3FileName(), true);
 Stream->play();
 }
+cout << "Destroying objects" << endl;
+return;
+}
+int main(int argc, char* argv[]) {
+CommandString = argv[1];
+int tid = _beginthread(Thread, 0, NULL);
+while (!Quit) Input();
+cout << "Quitting Daisy Player." << endl;
+Sleep(3000);
+return 0;
+}
+void Input() {
+char Key = getch();
+if (Key == 27 || Key == 'x' || Key == 'q') Quit = true;
 }
