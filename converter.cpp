208c887978a3b3c26e68b426edb855260bc647af
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2008-10-15 00:54:03 +0000

    Added two tags. Tagged LibDaisy-0.2 and added a Products-0.1 tag.
    git-svn-id: svn://mosedal.net/abf/code/tags/Products-0.1@143 3c19b6e9-aa5f-dd11-88a4-001302d38fef

---
+++ converter.cpp
@@ -0,0 +1,101 @@
+#include <libdaisy.h>
+#include <audiere.h>
+#include <speex/speex_resampler.h>
+#include <speex/speex.h>
+#include <iostream>
+#include <cstdio> // for FILE
+#include <cstdlib>
+#pragma comment(lib, "audiere.lib")
+#pragma comment(lib, "libdaisy.lib")
+#pragma comment(lib, "libspeexdsp_imp.lib")
+#pragma comment(lib, "libspeex_static.lib")
+using namespace ABF;
+using namespace std;
+using namespace audiere;
+void Decode(Daisy&, FILE*);
+void Encode(FILE*);
+int main(int argc, char* argv[]) {
+if (argc != 3) {
+cout << "Usage: " << argv[0] << " <path to daisy book> <output file>" << endl;
+return 1;
+}
+Daisy D(argv[1]);
+if (!D.IsValid()) {
+cout << "Error, not a valid daisy book." << endl;
+return 1;
+}
+FILE* fout = fopen(argv[2], "wb+");
+unsigned short Sections = D.GetNumSections();
+fwrite(&Sections, sizeof(short), 1, fout);
+int Size = 0;
+for (unsigned short i = 0; i < Sections; i++) {
+fwrite(&i, sizeof(short), 1, fout);
+fwrite(&Size, sizeof(int), 1, fout);
+}
+
+unsigned short Section = 0;
+while (D.OpenSmil()) {
+int Position = ftell(fout);
+fseek(fout, 2, SEEK_SET);
+for (int i = 0; i < Section; i++) fseek(fout, 6, SEEK_CUR);
+// We are now at the beginning of a section header.
+fseek(fout, 2, SEEK_CUR);
+cout << "Am to write a section size at position " << ftell(fout) << endl;
+fwrite(&Position, sizeof(int), 1, fout);
+fseek(fout, Position, SEEK_SET);
+Decode(D, fout);
+++Section;
+}
+fclose(fout);
+return 0;
+}
+void Decode(Daisy& D, FILE* fout) {
+unsigned int Size = 320, Processed = 320;
+short Resampled[320];
+SampleSourcePtr Source = OpenSampleSource(D.GetMP3FileName());
+int SampleRate, NumChannels;
+SampleFormat SF;
+Source->getFormat(NumChannels, SampleRate, SF);
+SpeexResamplerState* State = speex_resampler_init(1, SampleRate, 16000, 8, 0);
+short Buffer[320];
+FILE* temp = fopen("temp.raw", "wb");
+while (1) {
+unsigned int FramesRead = Source->read(320, Buffer);
+if (FramesRead <= 0) break;
+speex_resampler_process_int(State, 0, Buffer, &FramesRead, Resampled, &Processed);
+//Encode(Resampled, Processed, fout);
+fwrite(Resampled, sizeof(short), Processed, temp);
+}
+speex_resampler_destroy(State);
+fclose(temp);
+Encode(fout);
+return;
+}
+void Encode(FILE* fout) {
+FILE* fin = fopen("temp.raw", "rb");
+// Initialize Speex.
+void* Encoder = speex_encoder_init(&speex_wb_mode);
+SpeexBits Bits;
+speex_bits_init(&Bits);
+int FrameSize;
+speex_encoder_ctl(Encoder, SPEEX_GET_FRAME_SIZE, &FrameSize);
+short Input[400];
+unsigned short BytesToRead;
+while (!feof(fin)) {
+fread(Input, 2, 320, fin);
+speex_bits_reset(&Bits);
+speex_encode_int(Encoder, Input, &Bits);
+char Output[400];
+unsigned short Bytes = speex_bits_write(&Bits, Output, 400);
+fwrite(&Bytes, sizeof(short), 1, fout);
+fwrite(Output, sizeof(char), Bytes, fout);
+}
+speex_encoder_destroy(Encoder);
+speex_bits_destroy(&Bits);
+fclose(fin);
+#ifdef WIN32
+system("del temp.raw");
+#else
+system("rm temp.raw");
+#endif
+}
---
+++ player.cpp
@@ -0,0 +1,79 @@
+#include <process.h>
+#include <audiere.h>
+#include <speex/speex.h>
+#include <iostream>
+#include <cstdio>
+#include <conio.h>
+using namespace std;
+using namespace audiere;
+bool Quit = false;
+bool Previous = false;
+bool Next = false;
+void Thread(void* Filename) {
+void* Decoder = speex_decoder_init(&speex_wb_mode);
+SpeexBits Bits;
+speex_bits_init(&Bits);
+short Buffer[320];
+short Buffer1[32000];
+AudioDevicePtr Device(OpenDevice());
+FILE* Book = fopen((char*)Filename, "rb");
+if (!Book) {
+cout << "The book was not found." << endl;
+speex_bits_destroy(&Bits);
+speex_decoder_destroy(Decoder);
+return;
+}
+SampleFormat SF = SF_S16;
+OutputStreamPtr Stream;
+char Input[200];
+unsigned short Bytes;
+// We'll need to read our newly added headers.
+unsigned short NumSections;
+fread(&NumSections, sizeof(short), 1, Book);
+int* Array = new int[NumSections];
+for (int i = 0; i < NumSections; i++) {
+fseek(Book, 2, SEEK_CUR);
+fread(&Array[i], sizeof(int), 1, Book);
+}
+int CurrentSection = 0;
+while (!feof(Book) || Quit) {
+if (ftell(Book) > Array[CurrentSection+1]) CurrentSection += 1;
+if (Next) {
+CurrentSection += 1;
+fseek(Book, Array[CurrentSection], SEEK_SET);
+Next = false;
+}
+if (Previous) {
+CurrentSection -= 1;
+fseek(Book, Array[CurrentSection], SEEK_SET);
+Previous = false;
+}
+
+for (int i = 0; i < 32000; i+=320) {
+if (feof(Book)) break;
+fread(&Bytes, 2, 1, Book);
+fread(Input, 1, Bytes, Book);
+speex_bits_read_from(&Bits, Input, Bytes);
+speex_decode_int(Decoder, &Bits, Buffer);
+for (int j = 0; j < 320; j++) Buffer1[i+j] = Buffer[j];
+}
+Stream = Device->openBuffer(Buffer1, 32000, 1, 16000, SF);
+Stream->play();
+while (Stream->isPlaying());
+}
+speex_bits_destroy(&Bits);
+speex_decoder_destroy(Decoder);
+fclose(Book);
+delete[] Array;
+}
+void Input() {
+char Key = getch(); 
+if (Key == 'b') Next = true;
+if (Key == 'z') Previous = true;
+if (Key == 'q') Quit = true;
+}
+int main(int argc, char* argv[]) {
+_beginthread(Thread, 0, argv[1]);
+while (!Quit) Input();
+return 0;
+}
