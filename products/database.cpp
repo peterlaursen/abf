52a033ecb73abd321d4a805012dd309968eff93b
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2008-10-21 18:56:06 +0000

    The player can now save the last position of audio books.
    git-svn-id: svn://mosedal.net/abf/code/trunk@170 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- products/database.cpp
+++ products/database.cpp
@@ -3,18 +3,27 @@
 #include <string>
 #include "database.h"
 using namespace std;
+int Callback(void* Type, int NumRows, char** Results, char** Columns) {
+bool* Found = (bool*)Type;
+*Found = true;
+return 0;
+}
 void SaveLastPosition(FILE* fin, char* Title) {
 sqlite3* DB;
 sqlite3_open("ABFConverter.db", &DB);
 char Position[40];
 itoa(ftell(fin), Position, 10);
-string UpdateQuery = "update audiobooks set position=";
-UpdateQuery += Position;
-UpdateQuery += " where title='";
-UpdateQuery+=Title;
-UpdateQuery+="';";
-cout << "Update query: " << UpdateQuery << endl;
-sqlite3_exec(DB, UpdateQuery.c_str(), 0, 0, 0);
+string UpdateQuery = "select * from audiobooks where title = '";
+UpdateQuery += Title;
+UpdateQuery += "';";
+bool MyFound = false;
+bool* Found = &MyFound;
+sqlite3_exec(DB, UpdateQuery.c_str(), Callback, Found, 0);
+cout << boolalpha << MyFound << endl;
+if (MyFound) {
+cout << "The title already existed, updated position." << endl;
+return;
+}
 		string Query = "insert into audiobooks values('";
 Query += Title;
 Query += "', ";
