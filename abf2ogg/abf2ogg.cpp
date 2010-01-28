76752409c2098c93bbda6845a787579c74ce3c16
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2010-01-28 11:22:50 +0000

    Added an experimental program to trunk to convert ABF audio books to ogg files. It shall be included in 0.30-Alpha2.
    git-svn-id: svn://mosedal.net/abf/code/trunk@353 3c19b6e9-aa5f-dd11-88a4-001302d38fef

---
+++ abf2ogg/abf2ogg.cpp
@@ -0,0 +1,26 @@
+#include <libabf.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+using namespace ABF;
+using namespace std;
+int main(int argc, char* argv[]) {
+if (argc != 3) {
+cerr << "Usage: " << argv[0] << " <ABF Book> <output dir>" << endl;
+return 1;
+}
+AbfDecoder AD(argv[1]);
+int* Sections = AD.GetSections();
+FILE* Output = fopen("decoded.raw", "wb");
+short Decoded[320];
+cout << "Decoding " << argv[1] << endl;
+while (!AD.feof()) {
+AD.Decode(Decoded);
+fwrite(Decoded, sizeof(short), 320, Output);
+}
+fclose(Output);
+system("oggenc -r -C 1 -R 16000 -o abftest.ogg --quiet decoded.raw");
+cout << "Finished decoding." << endl;
+return 0;
+}
+
