257d7e768e69aba940ac6f94eb19d9917f562e94
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2012-05-26 18:28:15 +0000

    On FreeBSD, our ABF converter has undergone a few important changes: 1) We have installed a signal handler that handles the SIGINT signal. This means that, when we press Control-C, our converter removes all its temporary files, including the incomplete audio book. 2) Our converter no longer prints so much information as it did previously. It prints a line for each file converted and ends by stating its success.
    git-svn-id: svn://mosedal.net/abf/code/trunk@400 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- products/converter.cpp
+++ products/converter.cpp
@@ -1,3 +1,10 @@
+/* $Id$
+Copyright (C) 2009, 2010, 2011, 2012 Peter Laursen.
+
+This is a program to convert a Daisy book into an ABF file.
+It runs on Windows and FreeBSD.
+*/
+
 #ifdef WIN32
 #include <windows.h>
 #include <conio.h>
@@ -10,14 +17,34 @@
 #include <iostream>
 #include <cstdio> // for FILE
 #include <cstdlib>
+#ifndef WIN32
+#include <signal.h>
+#endif
+
+#ifdef WIN32
 #pragma comment(lib, "audiere.lib")
 #pragma comment(lib, "libdaisy.lib")
 #pragma comment(lib, "libspeexdsp.lib")
 #pragma comment(lib, "libspeex.lib")
 #pragma comment(lib, "libabf.lib")
+#endif
 using namespace ABF;
 using namespace std;
 using namespace audiere;
+#ifndef WIN32
+/*
+Since we define a signal handler for our FreeBSD systems, we store our 
+ABF audio book file name in this global variable.
+*/
+const char* AudioBookFileName = nullptr;
+void Cleanup(int Signal) {
+printf("Inside signal handler.\nCleaning up by removing temporary files. Removing temporary files.\n");
+system("rm /tmp/ABFConv*");
+unlink(AudioBookFileName);
+exit(Signal);
+}
+#endif
+
 int main(int argc, char* argv[]) {
 if (argc != 3) {
 cerr << "Usage: " << argv[0] << " <path to daisy book> <output file>" << endl;
@@ -28,9 +55,20 @@ if (!D.BookIsValid()) {
 cerr << "Error, not a valid daisy book." << endl;
 return 1;
 }
-AbfEncoder AE(argv[2]);
-D.GetMetadata();
+cout << "Before obtaining metadata." << endl;
+if (!D.GetMetadata()) {
+cerr << "Error: Cannot obtain metadata." << endl;
+return -1;
+}
+cout << "We have our metadata." << endl << "Before audio files." << 
+endl;
 D.GetAudioFiles();
+cout << "After metadata." << endl;
+AbfEncoder AE(argv[2]);
+#ifndef WIN32
+AudioBookFileName = argv[2];
+signal(SIGINT, &Cleanup);
+#endif
 AE.SetTitle(D.GetTitle().c_str());
 AE.SetAuthor(D.GetAuthor().c_str());
 AE.SetTime(D.GetTotalTime().c_str());
