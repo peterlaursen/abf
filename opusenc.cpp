724426a1d13af1791caee2066331b9e2714ed3d5
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2013-12-26 22:44:27 +0000

    Renamed opusenc.c to opusenc.cpp. Also, I modified our program a bit to read the file into memory. I hope I don't have a memory leak.
    git-svn-id: svn://mosedal.net/abf/code/trunk@410 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- opusenc.cpp
+++ opusenc.cpp
@@ -1,8 +1,9 @@
 /* We try to encode a very simple file. Afterwards, we try to decode the same file.
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <opus.h>
+#include <cstdio>
+#include <cstdlib>
+#include <sys/stat.h>
+#include <opus/opus.h>
 int main(int argc, char* argv[]) {
 if (argc != 2) {
 printf("Error, need at least an input file name.");
@@ -12,7 +13,19 @@ printf("Accepted.\n");
 // Let's try to create an encoder.
 int Error = 0;
 OpusEncoder* Encoder = opus_encoder_create(16000, 1, OPUS_APPLICATION_VOIP, &Error);
+// Get a little information about our encoder
+int Bitrate = 1;
+opus_encoder_ctl(Encoder, OPUS_SET_COMPLEXITY(&Bitrate));
+
+struct stat fileinfo;
+stat(argv[1], &fileinfo);
+
 FILE* fin = fopen(argv[1], "rb");
+printf("File size of input file: %lld\n", fileinfo.st_size);
+unsigned char* FileContents = new unsigned char[fileinfo.st_size];
+fread(FileContents, 1, fileinfo.st_size, fin);
+fclose(fin);
+fin = fmemopen(FileContents, fileinfo.st_size, "rb");
 short Buffer[320] = {0};
 FILE* fout = fopen("output.opus", "wb");
 while (!feof(fin)) {
@@ -21,13 +34,15 @@ int Samples = fread(Buffer, sizeof(short), 320, fin);
 unsigned short Length = opus_encode(Encoder, Buffer, Samples, OutputBuffer, 2048);
 
 if (Length > 100) break;
-printf("First frame: %d bytes\n", Length);
+//printf("First frame: %d bytes\n", Length);
 fwrite(&Length,1,sizeof(unsigned short),fout);
 fwrite(OutputBuffer, 1, Length, fout);
 }
 fclose(fout);
 fclose(fin);
+printf("Our bitrate is %d\n", Bitrate);
 opus_encoder_destroy(Encoder); 
-
+delete [] FileContents;
+FileContents = 0;
 }
 
