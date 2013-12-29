d03d3983bda9e2963d1592a1aef669edec14711a
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2013-12-29 14:53:13 +0000

    Make a copy of our opusenc.cpp so that we still have the original code if anything goes wrong with our encoding process using libabf.
    git-svn-id: svn://mosedal.net/abf/code/trunk@421 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- libabf/libabf.cpp
+++ libabf/libabf.cpp
@@ -5,7 +5,7 @@ using namespace std;
 namespace ABF {
 int SHARED AbfDecoder::Seek(long offset, int whence) { return fseek(fin, offset, whence); }
 AbfDecoder::AbfDecoder() {
-fin = 0;
+fin = nullptr;
 _IsOpen = false;
 }
 AbfDecoder::AbfDecoder(char* Filename) {
@@ -16,22 +16,36 @@ fin = fopen(Filename, "rb");
 if (!fin) _IsOpen = false;
 else _IsOpen = true;
 if (Validate()) ReadHeader();
-#ifndef WIN32
-Decoder = speex_decoder_init(&speex_wb_mode);
-#else
-Decoder = speex_decoder_init(speex_lib_get_mode(SPEEX_MODEID_WB));
-#endif
-speex_bits_init(&Bits);
+int Error = 0;
+Decoder = opus_decoder_create(16000, 1, &Error);
+if (Error != OPUS_OK) {
+printf("Something went wrong in creating our decoder!\n");
+::fclose(fin);
+_IsOpen = false;
+}
+
 }
 bool AbfDecoder::Validate() {
+rewind(fin);
 char Buffer[4];
 Buffer[3] = '\0';
 fread(Buffer, 1, 3, fin);
-if (strcmp(Buffer, "ABF") == 0) _IsValid = true;
-else _IsValid = false;
+unsigned short HeaderSize, Major, Minor;
+fread(&HeaderSize, sizeof(short), 1, fin);
+fread(&Major, sizeof(short), 1, fin);
+fread(&Minor, sizeof(short), 1, fin);
+
+if (strcmp(Buffer, "ABF") == 0 && HeaderSize > 0 && Major == 2 && Minor == 0) _IsValid = true;
+else {
+if (Major == 1) {
+printf("This file was encoded with an older version of libabf. We do not support that version of the format anymore. Please re-convert your ABF book.\n");
+}
+_IsValid = false;
+}
 return _IsValid;
 }
 void AbfDecoder::ReadHeader() {
+::fseek(fin, 3, SEEK_SET);
 fread(&HeaderSize, sizeof(short), 1, fin);
 fread(&Major, sizeof(short), 1, fin);
 fread(&Minor, sizeof(short), 1, fin);
@@ -51,7 +65,6 @@ Time[TitleLength] = '\0';
 fread(&NumSections, sizeof(short), 1, fin);
 Array = new int[NumSections];
 for (unsigned short i = 0; i < NumSections; i++) {
-fseek(fin, 2, SEEK_CUR);
 fread(&Array[i], sizeof(int), 1, fin);
 }
 return;
@@ -61,19 +74,21 @@ return Array;
 }
 void AbfDecoder::Decode(short* Output) {
 unsigned short Bytes;
-char Input[320];
+unsigned char Input[320];
 fread(&Bytes, 2, 1, fin);
-fread(Input, 1, Bytes, fin);
+int BytesRead = fread(Input, 1, Bytes, fin);
 if (feof()) return;
-speex_bits_read_from(&Bits, Input, Bytes);
-speex_decode_int(Decoder, &Bits, Output);
+
+int Error = opus_decode(Decoder, Input, BytesRead, Output, 320, 0);
 }
 int AbfDecoder::ftell() { return std::ftell(fin); }
 bool AbfDecoder::feof() { return std::feof(fin); }
-char* AbfDecoder::GetTitle() { return Title; }
-char* AbfDecoder::GetAuthor() { return Author; }
-char* AbfDecoder::GetTime() { return Time; }
-unsigned short AbfDecoder::GetNumSections() { return NumSections; }
+const char* AbfDecoder::GetTitle() const { return Title; }
+const char* AbfDecoder::GetAuthor() const { return Author; }
+const char* AbfDecoder::GetTime() const { return Time; }
+const unsigned short AbfDecoder::GetNumSections() const { return NumSections; }
+/* This function will not currently work - we therefore uncomment it until we can look at it more closely.
+
 bool AbfDecoder::GoToPosition(int Minutes) {
 // If input is wrong, bail out
 if (Minutes < 0) return false;
@@ -98,23 +113,26 @@ return false;
 Seek(SearchPosition, SEEK_SET);
 return true;
 }
+*/
 
 AbfDecoder::~AbfDecoder() {
 Reset();
 }
 void AbfDecoder::Reset() {
 if (_IsOpen) fclose();
-speex_decoder_destroy(Decoder);
-speex_bits_destroy(&Bits);
-	delete[] Title;
-	delete[] Author;
-	delete[] Time;
+opus_decoder_destroy(Decoder);
+delete[] Title;
+Title = nullptr;
+delete[] Author;
+Author = nullptr;
+delete[] Time;
+Time = nullptr;
 delete[] Array;
-
+Array = nullptr;
 }
 void AbfDecoder::fclose() { std::fclose(fin); }
 AbfEncoder::AbfEncoder() {
-fout = 0;
+fout = nullptr;
 }
 
 AbfEncoder::AbfEncoder(char* Filename) {
@@ -122,28 +140,27 @@ Initialize(Filename);
 }
 void AbfEncoder::Initialize(char* Filename) {
 fout = fopen(Filename, "wb+");
-#ifndef WIN32
-Encoder = speex_encoder_init(&speex_wb_mode);
-#else
-Encoder = speex_encoder_init(speex_lib_get_mode(SPEEX_MODEID_WB));
-#endif
-speex_bits_init(&Bits);
+int Error = 0;
+Encoder = opus_encoder_create(16000, 1, OPUS_APPLICATION_VOIP, &Error);
+if (Error != OPUS_OK) {
+printf("Error in creating our encoder!\n");
+}
+
 }
 AbfEncoder::~AbfEncoder() {
 fclose(fout);
-speex_encoder_destroy(Encoder);
-speex_bits_destroy(&Bits);
+opus_encoder_destroy(Encoder);
 }
 void AbfEncoder::SetTitle(const char* Title) { _Title = Title; }
 void AbfEncoder::SetAuthor(const char* Author) { _Author = Author; }
 void AbfEncoder::SetTime(const char* Time) { _Time = Time; }
 void AbfEncoder::SetNumSections(unsigned short NumSections) { _NumSections = NumSections; }
 void AbfEncoder::WriteHeader() {
-fseek(fout, 0, SEEK_SET);
+rewind(fout);
 fwrite("ABF", 1, 3, fout);
 HeaderSize = 0;
 fwrite(&HeaderSize, sizeof(short), 1, fout);
-unsigned short Major = 1, Minor = 0;
+unsigned short Major = 2, Minor = 0;
 fwrite(&Major, sizeof(short), 1, fout);
 fwrite(&Minor, sizeof(short), 1, fout);
 unsigned short Length = _Title.length();
@@ -162,7 +179,6 @@ fwrite(&HeaderSize, sizeof(short), 1, fout);
 fseek(fout, HeaderSize, SEEK_SET);
 int Size = 0;
 for (unsigned short i = 0; i < _NumSections; i++) {
-fwrite(&i, sizeof(short), 1, fout);
 fwrite(&Size, sizeof(int), 1, fout);
 }
 }
@@ -170,18 +186,15 @@ void AbfEncoder::WriteSection() {
 static int CurrentSection = 0;
 int Position = ftell(fout);
 fseek(fout, HeaderSize, SEEK_SET);
-for (int i = 0; i < CurrentSection; i++) fseek(fout, 6, SEEK_CUR);
+for (int i = 0; i < CurrentSection; i++) fseek(fout, 4, SEEK_CUR);
 // We are now at the beginning of a section header.
-fseek(fout, 2, SEEK_CUR);
 fwrite(&Position, sizeof(int), 1, fout);
 fseek(fout, Position, SEEK_SET);
 CurrentSection += 1;
 }
 void AbfEncoder::Encode(short* Input) {
-speex_bits_reset(&Bits);
-speex_encode_int(Encoder, Input, &Bits);
-char Output[200];
-unsigned short Bytes = speex_bits_write(&Bits, Output, 200);
+unsigned char Output[200] = {0};
+short Bytes = opus_encode(Encoder, Input, 320, Output, 200);
 fwrite(&Bytes, sizeof(short), 1, fout);
 fwrite(Output, sizeof(char), Bytes, fout);
 }
--- libabf/libabf.h
+++ libabf/libabf.h
@@ -1,8 +1,16 @@
+/* $Id$
+Copyright (C) 2010, 2011, 2012, 2013, 2014 Peter Laursen.
+
+This is our interface to our ABF library. Currently, we focus on upgrading our format slightly, so our player and other things may catch up as time progresses.
+For all the previous work on ABF, see /branches/libabf-1.0.
+That library is no longer updated and this library is incompatible with it.
+*/
+
 #ifndef LIBABF_H
 #define LIBABF_H
 #include <cstdio>
 #include <string>
-#include <speex/speex.h>
+#include <opus/opus.h>
 #ifdef WIN32
 #ifdef BUILD_DLL
 #define SHARED __declspec(dllexport)
@@ -17,47 +25,47 @@ using std::FILE;
 using std::string;
 class SHARED AbfDecoder {
 int* Array;
-	void* Decoder;
-SpeexBits Bits;
-FILE* fin;
-bool _IsOpen;
-bool _IsValid;
-char* Title;
-char* Author;
-char* Time;
-unsigned short HeaderSize, Major, Minor, NumSections;
+OpusDecoder* Decoder = nullptr;
+FILE* fin = nullptr;
+bool _IsOpen = false;;bool _IsValid;
+char* Title = nullptr;
+char* Author = nullptr;
+char* Time = nullptr;
+unsigned short HeaderSize = 0, Major = 0, Minor = 0, NumSections = 0;
 void ReadHeader();
 bool Validate();
 public:
 AbfDecoder(char* Filename);
 AbfDecoder();
 ~AbfDecoder();
-void Initialize(char*);
+void Initialize(char* Filename);
 void Reset();
-bool IsValid() { return _IsValid; }
-bool IsOpen() { return _IsOpen; }
- char* GetTitle();
- char* GetAuthor();
- char* GetTime();
-unsigned short GetNumSections();
-unsigned short GetHeaderSize() { return HeaderSize; }
-unsigned short GetMajor();
-unsigned short GetMinor();
+bool IsValid() const { return _IsValid; }
+bool IsOpen() const { return _IsOpen; }
+const char* GetTitle() const;
+const char* GetAuthor() const;
+const char* GetTime() const;
+const unsigned short GetNumSections() const;
+const unsigned short GetHeaderSize() const { return HeaderSize; }
+const unsigned short GetMajor();
+const unsigned short GetMinor() const;
 bool feof();
 int ftell();
 void fclose();
 int Seek(long offset, int whence);
 int* GetSections();
 void Decode(short* Output);
+/*
+This function currently will not work since our format has changed quite a bit. I'll see what I can do about it.
+*/
 bool GoToPosition(int Minutes);
 };
 class SHARED AbfEncoder {
-void* Encoder;
-SpeexBits Bits;
-FILE* fout;
-unsigned short HeaderSize;
+OpusEncoder* Encoder = nullptr;
+FILE* fout = nullptr;
+unsigned short HeaderSize = 0;
 string _Title, _Author, _Time;
-unsigned short _NumSections;
+unsigned short _NumSections = 0;
 public:
 AbfEncoder(char* Filename);
 AbfEncoder();
