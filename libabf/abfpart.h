c6bb49bacaca40be717d7192b8b3d9e1f2725f4b
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2016-08-23 11:44:37 +0000

    Alter our AbfPart implementation a bit. Also add our first attempt at a write function.
    git-svn-id: svn://mosedal.net/abf/code/trunk@598 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- libabf/abfpart.h
+++ libabf/abfpart.h
@@ -7,26 +7,30 @@ It contains various flags and other housekeeping information for the ABF encoder
 #ifndef ABFPART_H
 #define ABFPART_H
 #include <iosfwd>
-#include <iostream>
 #include <fstream>
 #include <string>
+#include <opus/opus.h>
+
 namespace ABFMulti {
 using std::ofstream;
 using std::string;
+using std::ios_base;
 enum AbfPartStatus {
 Waiting,
 Encoding,
 Finished,
 };
 class AbfPart {
+OpusEncoder* Encoder = nullptr;
 ofstream PartFile;
 unsigned int Sequence; // number of this part.
 AbfPartStatus Status = Waiting;
 public:
-AbfPart(unsigned int Sequence, const string FileName): Sequence(Sequence), PartFile(FileName), Status(Waiting) {}
+AbfPart(unsigned int Sequence, const string FileName);
 ~AbfPart();
 AbfPartStatus GetStatus() const { return Status; }
 unsigned int GetPartNumber() const { return Sequence; }
+void Write(short* Array);
 };
 }
 #endif
