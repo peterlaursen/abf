501290e79d8b230c4d23bbb11fb5c48accb97148
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2018-01-21 11:45:19 +0000

    Remove Windows and Linux specific things from this player folder.
    git-svn-id: svn://mosedal.net/abf/code/trunk@644 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- newplayer/player.h
+++ newplayer/player.h
@@ -1,27 +1,13 @@
 /* $Id$
-Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015 Peter Laursen.
+Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 Peter Laursen.
 
 This file contains various declarations that are used within the player.
 */
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "../libabf/libabf.h"
-#ifdef WIN32
-#include <windows.h>
-#include "dsaudio.h"
-#else
 #include "unixaudio.h"
-#endif
-
-#ifdef WIN32
-typedef HANDLE ThreadType;
-#endif
-#ifdef LINUX
-typedef pthread_t ThreadType;
-#endif
-#ifdef FREEBSD
 typedef pthread* ThreadType;
-#endif
 enum PlayerStatus {
 Nothing = 0, // A default value only to be used in the beginning
 Playing,
