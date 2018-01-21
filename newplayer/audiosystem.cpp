0d5bb8901e37d8d8a1141fe71b00f5a3e1300aa7
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2018-01-21 11:26:14 +0000

    Bump copyright years for the audio system classes.
    git-svn-id: svn://mosedal.net/abf/code/trunk@642 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- newplayer/audiosystem.cpp
+++ newplayer/audiosystem.cpp
@@ -1,5 +1,5 @@
 /* $Id$
-Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015 Peter Laursen.
+Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 Peter Laursen.
 
 This contains the implementation of the AudioSystem class.
 */
