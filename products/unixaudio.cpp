f500e2b16524c071c42bbfdd5339ad14c8927faf
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2018-06-08 09:43:04 +0000

    Copy Unixaudio.{h,cpp} from newplayer. This was apparently never merged back to the main player.
    git-svn-id: svn://mosedal.net/abf/code/trunk@675 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- products/unixaudio.cpp
+++ products/unixaudio.cpp
@@ -1,10 +1,10 @@
 /* $Id$
-Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015 Peter Laursen.
+Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 Peter Laursen.
 
 This file contains the class implementation of the OSS API that's in use on FreeBSD.
 If your system uses another audio system, derive from the AudioSystem class and make your own implementation.
 */
-#include <stdio.h>
+#include <cstdio>
 #include "unixaudio.h"
 #include "audiosystem.h"
 #include "player.h"
@@ -14,24 +14,38 @@ If your system uses another audio system, derive from the AudioSystem class and 
 #include <fcntl.h>
 #include <unistd.h>
 namespace ABF {
-UnixAudio::UnixAudio() {
+UnixAudio::UnixAudio(): Device(0), Volume(0) {
+/* If the preprocessor symbol BLUETOOTH is defined, we open the device /dev/dspbt
+This is mainly for use with the virtual_oss port.
+If not, we open the standard audio device (/dev/dsp).
+*/
+
 #ifdef BLUETOOTH
 Device = open("/dev/dspbt", O_WRONLY);
 #else
 Device = open("/dev/dsp", O_WRONLY);
 #endif
-int Format = AFMT_S16_NE;
+}
+void UnixAudio::Init(AbfDecoder* _AD) {
+AD = _AD;
+int Format = 1;
+ioctl(Device, SNDCTL_DSP_COOKEDMODE, &Format);
+Format = AFMT_S16_NE;
 ioctl(Device, SNDCTL_DSP_SETFMT, &Format);
 Format = 1;
 ioctl(Device, SNDCTL_DSP_CHANNELS, &Format);
 Format = 16000;
 ioctl(Device, SNDCTL_DSP_SPEED, &Format);
+if (Format != 16000) {
+AD->SetSamplingRate(Format);
+FrameSize = 960;
+}
 }
 UnixAudio::~UnixAudio() { close(Device); }
 void UnixAudio::Play() {
 IsPlaying = true;
 while (!AD->feof() && PS == Playing) {
-short Buffer[320];
+short Buffer[FrameSize];
 AD->Decode(Buffer);
 write(Device, Buffer, sizeof(Buffer));
 }
@@ -51,8 +65,10 @@ ioctl(Device, SNDCTL_DSP_GETPLAYVOL, &Volume);
 int LeftVolume = 0, RightVolume = 0;
 LeftVolume = Volume & 0x7f;
 RightVolume = Volume >> 8 & 0x7f;
+
 if (LeftVolume == 0) return;
 LeftVolume = RightVolume -= 5;
+
 Volume = LeftVolume|(RightVolume<<8);
 ioctl(Device, SNDCTL_DSP_SETPLAYVOL, &Volume);
 }
