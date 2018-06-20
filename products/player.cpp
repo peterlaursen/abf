1e1efdafe8c0a60e86e9461d287c55c789692066
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2018-06-20 09:03:50 +0000

    Revert R685.
    git-svn-id: svn://mosedal.net/abf/code/trunk@688 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- products/player.cpp
+++ products/player.cpp
@@ -370,8 +370,7 @@ int main(int argc, char* argv[]) {
 if (argc < 2) AddBookToPlaylist();
 #ifndef WIN32
 cfmakesane(&oldt);
-newt.c_lflag &= ~ICANON;
-newt.c_flag &= ~ECHO;
+cfmakeraw(&newt);
 tcsetattr(0, TCSANOW, &newt);
 glob_t g;
 for (int i = 1; i < argc; i++) {
