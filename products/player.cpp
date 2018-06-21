3f75185c8d4f1c67bbdc6bdf81e20feed3a716e3
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2018-06-21 16:40:07 +0000

    Fix globbing.
    git-svn-id: svn://mosedal.net/abf/code/trunk@706 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- products/player.cpp
+++ products/player.cpp
@@ -386,11 +386,13 @@ DevName = argv[2];
 }
 #ifndef WIN32
 glob_t g;
+bool Globbed = false;
 for (int i = Start; i < argc; i++) {
 glob(argv[i], GLOB_BRACE|GLOB_TILDE, NULL, &g);
+Globbed = true;
 for (int i = 0; i < g.gl_pathc; i++) PL.Add(g.gl_pathv[i]);
 }
-if (!g) globfree(&g);
+if (Globbed) globfree(&g);
 #endif
 #ifdef GPIO
 devhandle = gpio_open(0);
