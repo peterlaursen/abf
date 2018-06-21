bed45580d25dcb0830733b02234361a1a59294fd
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2018-06-21 16:36:08 +0000

    Fix syntax.
    git-svn-id: svn://mosedal.net/abf/code/trunk@705 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- products/player.cpp
+++ products/player.cpp
@@ -385,7 +385,7 @@ DevName = argv[2];
 }
 }
 #ifndef WIN32
-glob_t g = nullptr;
+glob_t g;
 for (int i = Start; i < argc; i++) {
 glob(argv[i], GLOB_BRACE|GLOB_TILDE, NULL, &g);
 for (int i = 0; i < g.gl_pathc; i++) PL.Add(g.gl_pathv[i]);
