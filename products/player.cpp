a73b9ae63cd54c65e7c76275315f224a91a38b3c
Peter Laursen <46241512+peterlaursen@users.noreply.github.com>
2018-12-29 12:42:59 +0000

    Cherrypick a small change to player.cpp so that we can still seek in the latest encoded formats.
    git-svn-id: svn://mosedal.net/abf/code/branches/win32@809 3c19b6e9-aa5f-dd11-88a4-001302d38fef

--- products/player.cpp
+++ products/player.cpp
@@ -32,7 +32,7 @@ PL.Remove(PL.GetCurrentBook());
 if (PL.GetTotalItems() == 0) return;
 }
 bool JumpToTime(AbfDecoder& AD) {
-if (AD.GetMajor() == 2 && AD.GetMinor() != 1) {
+if (AD.GetMajor() == 2 && AD.GetMinor() < 1) {
 cerr << "ABF " << AD.GetMajor() << "." << AD.GetMinor() << " does not support seeking." << endl;
 cerr << "You must re-encode with a supported encoder in order to get this ability." << endl;
 return false;
